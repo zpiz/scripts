8cb1056c958a07e57a85cd628922f3c0efd46d7a
zpiz <134020066+zpiz@users.noreply.github.com>
2025-10-14 09:57:50 +0800

    Update dlmck.js

--- dlmck.js
+++ dlmck.js
@@ -1,7 +1,7 @@
 // Quantumult X 脚本：达美乐ck自动获取
 /*
 [rewrite_local]
-^https:\/\/game\.dominos\.com\.cn\/[^\/]+\/v2\/\/getUser\?openid=undefined url script-request-header https://raw.githubusercontent.com/gitk01n/quantumultx/refs/heads/main/script/dlmck.js
+^https:\/\/game\.dominos\.com\.cn\/[^\/]+\/v2\/getUser\?openid=undefined url script-request-header https://raw.githubusercontent.com/gitk01n/quantumultx/refs/heads/main/script/dlmck.js
 [MITM]
 hostname = game.dominos.com.cn
 */
