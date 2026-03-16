a4baf47f9184d212da96a65a618af8adbbadb052
zpiz <134020066+zpiz@users.noreply.github.com>
2026-03-16 15:30:20 +0800

    Fix URL encoding for primary oil price API

--- Egern/oil-price.js
+++ Egern/oil-price.js
@@ -3,7 +3,7 @@ export default async function(ctx) {
   const cacheKey = `oil_price_cache_${province}`;
   
   // 引入主备双路 API 机制，提升可用性
-  const primaryUrl = `http://m.qiyoujiage.com/${encodeURIComponent(province)}.shtml`;
+  const primaryUrl = `http://m.qiyoujiage.com/${province}.shtml`;
   //const primaryUrl = `https://tenapi.cn/v2/oil?province=${encodeURIComponent(province)}`;
   const backupUrl = `https://api.vvhan.com/api/oil?prov=${encodeURIComponent(province)}`;
   
